21d90d2c709d068ba465f7619aa6fe86c95af7b1
Pascal Bürkle <exxxodus.artz@gmail.com>
2021-08-07 18:31:42 +0200

    improve steps column layout on index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -89,29 +89,54 @@
     </div>
   
     <div class="flex flex-col flex-1">
-      <h2 class="font-bold text-2xl mb-6">Steps</h2>
-  
-      <div class="flex mb-5 items-center justify-between px-4">
-        <div class="flex justify-between">
-          <button on:click={decreaseStepCount} class="hover:text-primary-clamped">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
-            </svg>
-          </button>
-          <div class="tabular-nums w-10">
-            <input
-              type="text"
-              class="bg-transparent w-full text-center focus:outline-none text-lg"
-              bind:value={stepsCount}
-            />
+      <div class="flex items-center justify-between mb-6">
+        <h2 class="font-bold text-2xl">Steps</h2>
+
+        <div class="flex items-center">
+          <div class="flex justify-between">
+            <button on:click={decreaseStepCount} class="hover:text-primary-clamped">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+              </svg>
+            </button>
+            <div class="tabular-nums w-8">
+              <input
+                type="text"
+                class="bg-transparent w-full text-center focus:outline-none text-lg"
+                bind:value={stepsCount}
+                min={colorInstances.length}
+                max="50"
+              />
+            </div>
+            <button on:click={increaseStepCount} class="hover:text-primary-clamped">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                />
+              </svg>
+            </button>
           </div>
-          <button on:click={increaseStepCount} class="hover:text-primary-clamped">
+    
+          <button
+            on:click={() => (showStepsAsGradient = !showStepsAsGradient)}
+            style="color: {!showStepsAsGradient ? 'var(--color-primary)' : 'currentColor'}"
+            class="ml-5"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-6 w-6"
@@ -123,32 +148,13 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
           </button>
         </div>
-  
-        <button
-          on:click={() => (showStepsAsGradient = !showStepsAsGradient)}
-          style="color: {!showStepsAsGradient ? 'var(--color-primary)' : 'currentColor'}"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-            />
-          </svg>
-        </button>
       </div>
+      
   
       {#if showStepsAsGradient}
         <div class="h-full rounded-2xl" style="background: linear-gradient(to bottom, {gradient});" />
