121f0038ae9c906dd8b16803237aa251275b845e
Pascal Bürkle <exxxodus.artz@gmail.com>
2021-08-22 18:18:16 +0200

    fix click event on color block info icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -191,7 +191,7 @@
     <div class="flex flex-col flex-1">
       <h2 class="font-bold text-2xl mb-6">Mixed</h2>      
       <ColorBlock color={averageColor} expands alwaysShowColor className="rounded-2xl">
-        <a href="/info?color={averageColor.hex().replace('#', '')}" class="absolute bottom-4 right-4 opacity-70 transition hover:opacity-100">
+        <a on:click|stopPropagation={() => {}} href="/info?color={averageColor.hex().replace('#', '')}" class="absolute bottom-4 right-4 opacity-70 transition hover:opacity-100">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
