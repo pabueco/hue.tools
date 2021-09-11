b62942517d79f75dab2b2fe30370b363d1aea430
Pascal Bürkle <exxxodus.artz@gmail.com>
2021-09-11 19:16:45 +0200

    add compact prop to ColorBlock

--- src/components/ColorBlock.svelte
+++ src/components/ColorBlock.svelte
@@ -14,6 +14,7 @@
 	export let className: string = '';
 	export let size: string = 'default';
 	export let showName: boolean = false;
+	export let compact: boolean = false;
 
 	let textColor = undefined;
 
@@ -34,19 +35,25 @@
 
 <div
 	style="background: {color.hex()}; color: {textColor}"
-	class="group relative min-h-[2.5rem] cursor-pointer transition duration-200 flex items-center justify-center select-none whitespace-nowrap {expands
-		? 'w-full flex-1'
-		: 'w-10'} {animatesOnHover ? 'transform hover:scale-110' : ''} {animatesOnClick
-		? 'active:scale-90'
-		: ''} {className} {size === 'sm' ? 'text-sm' : ''} {size === 'lg' ? 'text-lg' : ''}"
-	on:click={(e) => copyToClipboard(e, color.toString($outputFormat))}
+	class="group relative min-h-[2.5rem] cursor-pointer transition duration-200 flex items-center justify-center select-none whitespace-nowrap 
+    {expands ? 'w-full flex-1' : 'w-10'} 
+    {animatesOnHover ? 'transform hover:scale-110' : ''} 
+    {animatesOnClick ? 'active:scale-90' : ''} 
+    {className} 
+    {size === 'sm' ? 'text-sm' : ''} 
+    {size === 'lg' ? 'text-lg' : ''}
+    {compact ? 'flex-col' : ''}
+  "
+	on:click|self={(e) => copyToClipboard(e, color.toString($outputFormat))}
 	on:mouseenter={() => (!alwaysShowColor ? (textColor = color.textColor()) : null)}
 >
 	{#if showName && colorName}
 		<div
-			class="absolute top-4 text-lg font-medium opacity-80 transition hover:opacity-100 left-1/2 transform -translate-x-1/2 text-center"
+			class="text-lg font-medium opacity-80 transition hover:opacity-100 transform text-center
+      {compact ? 'mb-1' : 'absolute top-4 left-1/2 -translate-x-1/2'}
+      "
 			style="color: {textColor};"
-			on:click={(e) => copyToClipboard(e, colorName)}
+			on:click={(e) => copyToClipboard(e, colorName, 'Name copied!')}
 		>
 			{colorName}
 		</div>
@@ -55,7 +62,7 @@
 	<span
 		class="{!alwaysShowColor
 			? 'opacity-0 scale-75'
-			: ''} transform group-hover:opacity-100 group-hover:scale-100 font-medium"
+			: ''} transform group-hover:opacity-100 group-hover:scale-100 font-medium pointer-events-none"
 		style="transition: opacity 150ms, transform 150ms;">{color.toString($outputFormat)}</span
 	>
 
