35ff1fad40ad1e4e3a8791b522ac322cd7692df3
Pascal Bürkle <exxxodus.artz@gmail.com>
2021-09-12 17:52:57 +0200

    fix color of github icon in footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -47,7 +47,9 @@
 
 	<div class="bg-gray-900 rounded-2xl mb-3 relative z-10 text-white">
 		<div class="px-6 md:px-10 py-6 flex flex-col justify-center relative">
-			<div class="relative md:absolute md:inset-x-10 w-full md:w-auto flex items-center justify-between">
+			<div
+				class="relative md:absolute md:inset-x-10 w-full md:w-auto flex items-center justify-between"
+			>
 				<div class="flex items-center">
 					<a href="/" class="text-2xl md:text-3xl font-medium text-primary-clamped">hue.tools</a>
 					<button
@@ -106,7 +108,7 @@
 				</div>
 			</div>
 
-      <div class="bg-gray-800 block md:hidden h-px my-5 -mx-6"></div>
+			<div class="bg-gray-800 block md:hidden h-px my-5 -mx-6" />
 
 			<div
 				class="self-center flex items-center justify-center space-x-4 text-xl font-medium order-last flex-basis-[100%] md:order-none relative z-10"
@@ -128,15 +130,26 @@
 		class="flex items-center justify-center space-x-4 pt-4 relative z-10"
 		style="color: {new Color($complementColor).textColor()}"
 	>
-    <a href="https://github.com/pabueco/hue.tools" target="_blank">
-      <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
-        <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-    </svg>
-    </a>
+		<a href="https://github.com/pabueco/hue.tools" target="_blank">
+			<svg
+				height="24"
+				aria-hidden="true"
+				viewBox="0 0 16 16"
+				version="1.1"
+				width="24"
+				data-view-component="true"
+        fill="currentColor"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+				/>
+			</svg>
+		</a>
 		<div>
-      <span>Made by</span>
-		  <a href="https://pabue.co" target="_blank" class="border-b border-current">pabue.co</a>
-    </div>
+			<span>Made by</span>
+			<a href="https://pabue.co" target="_blank" class="border-b border-current">pabue.co</a>
+		</div>
 	</div>
 </div>
 
