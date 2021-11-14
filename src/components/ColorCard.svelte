208bb6cbc270be8878e87df339bee3c8e5bda808
Pascal Bürkle <exxxodus.artz@gmail.com>
2021-11-14 13:55:26 +0100

    add copy button next to input in ColorCard

--- src/components/ColorCard.svelte
+++ src/components/ColorCard.svelte
@@ -120,7 +120,7 @@
 	</button>
 
 	<div
-		class="bg-black bg-opacity-25 text-white hover:bg-opacity-50 focus:bg-opacity-50 rounded-3xl transition mt-12 {isColorPickerVisible
+		class="bg-black bg-opacity-25 text-white hover:bg-opacity-50 focus:bg-opacity-50 rounded-3xl transition mt-12 relative {isColorPickerVisible
 			? 'bg-opacity-50'
 			: ''}"
 	>
@@ -142,5 +142,26 @@
 			on:blur={(e) => onColorInput(e)}
 			on:keyup={(e) => e.key === 'Enter' && onColorInput(e)}
 		/>
+     
+    <button
+      on:click={(e) => copyToClipboard(e, formattedColorValue, 'Copied!')}
+      class="transition text-white opacity-60 hover:opacity-100 absolute left-[18px] bottom-[11px]"
+      title="Copy"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+        />
+      </svg>
+    </button>
 	</div>
 </div>
