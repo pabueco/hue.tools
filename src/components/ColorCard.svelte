6a779d9b8f1db7fee9d9785c3ffd927ea609dc62
KappaPrajd <tomaszmolski22@wp.pl>
2022-02-23 22:51:36 +0100

    chore: changed warning icon to ensure consistency

--- src/components/ColorCard.svelte
+++ src/components/ColorCard.svelte
@@ -194,12 +194,11 @@
       
       {:else}
       <button
-          class="absolute left-[20px] bottom-[2px]"
+          class="absolute left-[18px] bottom-[11px]"
           title="Invalid color"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="red" class="h-8 w-8" viewBox="0 0 24 24" stroke="red">
-          <path stroke-width="0.7" d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-          <path stroke-width="0.7" d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="red">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
         </svg>
       </button>
     {/if}
